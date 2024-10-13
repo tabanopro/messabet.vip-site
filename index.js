78f7675789598f1dfeeb6bc33a03e1496a9833f9
tabanopro <82659377+tabanopro@users.noreply.github.com>
2024-10-13 16:50:22 +0000

    e

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.get("/", (req, res) => {
+    res.send("./index.html");
+})
+
+app.listen(3000, ()=> {
+    console.log("Server started on port 3000");
+})
